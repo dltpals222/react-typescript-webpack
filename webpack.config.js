09a9c5d8d0ed33286075ff30b6400a4a99745489
leesemin <leesemin222@gmail.com>
2023-06-07 21:03:00 +0900

    모듈방식을 commonJS로 해야 에러가 안뜬다

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 const isProduction = process.env.NODE_ENV === "production";
 const config = {
   entry: path.resolve(__dirname, "develop", "router.tsx"),
   output: path.resolve(__dirname, "dist"),
   mode: isProduction ? "production" : "developmenet",
-  plugins: [new HtmlWebpackPlugin({})],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "dist", "index.html"),
+    }),
+    new webpack.ProvidePlugin({
+      React: "react",
+    }),
+  ],
 };
