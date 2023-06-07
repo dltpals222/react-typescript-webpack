dbd2a27b079afed18b8b829d4ff04374e01d4b15
leesemin <leesemin222@gmail.com>
2023-06-07 21:43:13 +0900

    webpack.config.js 수동작성 완료

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 const webpack = require("webpack");
 
 const isProduction = process.env.NODE_ENV === "production";
@@ -14,5 +15,49 @@ const config = {
     new webpack.ProvidePlugin({
       React: "react",
     }),
+    new webpack.HotModuleReplacementPlugin(),
   ],
+  module: {
+    rule: [
+      {
+        test: /\.(tsx?|js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "ts-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true, //콘솔로그 제거
+          },
+          output: {
+            comments: true, //주석 제거
+          },
+        },
+      }),
+    ],
+  },
+  devServer: {
+    historyApiCallback: true,
+    port: 3000,
+    hot: true,
+    inline: true,
+    publicPath: "/",
+  },
 };
+
+module.exports = config;
