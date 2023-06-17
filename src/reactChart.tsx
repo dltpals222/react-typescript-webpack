c7db6d38392ecbc994a27c73b62c3d8385ad207b
leesemin <leesemin222@gmail.com>
2023-06-18 03:11:22 +0900

    마리아db 연결 모듈 만들기

--- src/reactChart.tsx
+++ src/reactChart.tsx
@@ -1,10 +1,15 @@
-import * as reactFinancialCharts from 'react-financial-charts'
+import {ChartCanvas, Chart, CandlestickSeriesProps, XAxis, YAxis} from 'react-financial-charts'
 import * as React from 'react'
 
 const financialChart = () => {
   return (
     <>
-      
+      <h1>파이낸셜 차트</h1>
+      <ChartCanvas>
+        <Chart>
+          
+        </Chart>
+      </ChartCanvas>
     </>
   )
 }
